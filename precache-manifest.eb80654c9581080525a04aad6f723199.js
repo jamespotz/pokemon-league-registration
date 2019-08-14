e7e134a5e8892ff926b0daeb0a00845844ba4885
James Robert Rooke <thepoltergeist23@gmail.com>
2019-08-14 14:59:41 +0800

    Updates

---
+++ precache-manifest.eb80654c9581080525a04aad6f723199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab8a01a36b0e85d04b80e86fa20246f",
+    "url": "/pokemon-league-registration/index.html"
+  },
+  {
+    "revision": "d53917f63491c2c5b60f",
+    "url": "/pokemon-league-registration/static/css/main.05dbcfc1.chunk.css"
+  },
+  {
+    "revision": "d38b44e76935cbe744bd",
+    "url": "/pokemon-league-registration/static/js/2.24d0221b.chunk.js"
+  },
+  {
+    "revision": "d53917f63491c2c5b60f",
+    "url": "/pokemon-league-registration/static/js/main.eda7523a.chunk.js"
+  },
+  {
+    "revision": "b804673b28edc2e22011",
+    "url": "/pokemon-league-registration/static/js/runtime~main.cfa55a75.js"
+  }
+]);
